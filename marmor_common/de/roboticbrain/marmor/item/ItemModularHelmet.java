0d34a25104274bf423c209871a49dcc2d73fd349
Robotic-Brain <github@roboticbrain.de>
2013-04-19 00:36:09 +0200

    Used Arrow Icon for Item

--- marmor_common/de/roboticbrain/marmor/item/ItemModularHelmet.java
+++ marmor_common/de/roboticbrain/marmor/item/ItemModularHelmet.java
@@ -2,8 +2,12 @@ package de.roboticbrain.marmor.item;
 
 import java.util.logging.Level;
 
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import de.roboticbrain.marmor.Constants;
@@ -19,10 +23,11 @@ import de.roboticbrain.marmor.lib.Strings;
  * @author Robotic-Brain
  * @license GNU Public License v3 (http://www.gnu.org/licenses/)
  */
-public class ItemModularHelmet extends Item {
+public class ItemModularHelmet extends ItemArmor {
 
     public ItemModularHelmet(int id) {
-        super(id);
+        // Id, material, renderIndex, armorType
+        super(id, EnumArmorMaterial.IRON, 1, 0);
         this.setMaxStackSize(1);
         this.setCreativeTab(ModularArmorMod.creativeTab);
         this.setUnlocalizedName(Strings.MODULAR_HELMET_NAME);
@@ -31,7 +36,8 @@ public class ItemModularHelmet extends Item {
     @Override
     @SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister iconRegister) {
-        itemIcon = iconRegister.registerIcon(Constants.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+        //itemIcon = iconRegister.registerIcon(Constants.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+        itemIcon = Item.arrow.getIcon(new ItemStack(arrow), 0);
     }
     
 }
