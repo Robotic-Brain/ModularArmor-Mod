5118259b15bc60b087f66a63685ae9bc205f837f
Robotic-Brain <github@roboticbrain.de>
2013-04-18 20:24:40 +0200

    Added Mod Entry Point Added LogHelper (Original from EE3)

---
+++ marmor_common/de/roboticbrain/marmor/Constants.java
@@ -0,0 +1,15 @@
+package de.roboticbrain.marmor;
+
+/**
+ * Modular Armor Constants
+ * 
+ * @author Robotic-Brain
+ * @license GNU Public License v3 (http://www.gnu.org/licenses/)
+ *
+ */
+public class Constants {
+    public static final String MODID = "ModularArmorMod";
+    public static final String VERSION_STRING = "0.0.1";
+    
+    public static final String CHANNEL_NAME = MODID;
+}
---
+++ marmor_common/de/roboticbrain/marmor/ModularArmorMod.java
@@ -0,0 +1,46 @@
+package de.roboticbrain.marmor;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import de.roboticbrain.marmor.core.helper.LogHelper;
+
+/**
+ * Modular Armor Mod Entry Point
+ * 
+ * @author Robotic-Brain
+ * @license GNU Public License v3 (http://www.gnu.org/licenses/)
+ *
+ */
+@Mod(modid = Constants.MODID, version = Constants.VERSION_STRING)
+/*@NetworkMod(
+            channels = { Constants.CHANNEL_NAME },
+            clientSideRequired = true,
+            serverSideRequired = false
+        )*/
+public class ModularArmorMod {
+    @Instance(Constants.MODID)
+    public static ModularArmorMod instance;
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        LogHelper.init();
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        LogHelper.logger().info("Initialization finished");
+    }
+    
+}
